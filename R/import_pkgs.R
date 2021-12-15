48516daf9b0d6832c480270f55673d2971e3cc24
Song Zhai <zsviolet1993@gmail.com>
2021-12-15 18:50:07 +0000

    version 0.1.0

---
+++ R/import_pkgs.R
@@ -0,0 +1,17 @@
+#' @import mvtnorm
+#' @import GIGrvg
+#' @import MCMCpack
+#' @import Matrix
+#' @import bdsmatrix
+#' @import gglasso
+#' @import glmnet
+#' @import SGL
+#' @import propagate
+#' @import bigsnpr
+#' @import bigsparser
+#' @import lmtest
+#' @import bigstatsr
+#' @importFrom methods as
+#' @importFrom stats coef cor lm rgamma rnorm sd cov
+#' @importFrom bigparallelr nb_cores
+NULL
